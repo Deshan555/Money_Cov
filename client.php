8fe51b6b51205a9ada76953732f1bb2e2f13fa80
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-15 08:54:49 +0530

    fix bugs with client code

---
+++ client.php
@@ -0,0 +1,54 @@
+<?php
+
+class data_info
+{
+    public $source_currency;
+
+    public $amount;
+
+    public $target_currency;
+}
+
+class Client{
+
+    public $instance = NULL;
+
+
+    public function __construct()
+    {
+        $params = array(
+            'location'=>'http://localhost/Test/example.php?wsdl',
+            'uri' =>  'urn://localhost/Test/example.php?wsdl'  ,
+            'trace'=>1,'cache_wsdl'=>WSDL_CACHE_NONE    );
+        $this->instance =  new SoapClient(NULL, $params);
+    }
+
+    public function money_conv($data_array)
+    {
+        return $this->instance->__soapCall('Convert_Money', [$data_array]);
+    }
+
+}
+
+$client = new Client;
+
+$data_array = new data_info();
+
+$data_array->source_currency = 'LKR';
+
+$data_array->amount = 1000;
+
+$data_array->target_currency = 'AFN';
+
+try
+{
+    echo "Money Conversion : \t: " , $client->money_conv($data_array),"\n";
+
+    echo "Done\n";
+}
+catch (Exception $e)
+{
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+?>
